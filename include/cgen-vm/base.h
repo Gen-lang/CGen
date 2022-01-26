5d3fb0744e73ce68ea786762cb9c76efcc6d15f4
GalaxianMonster <72903778+GalaxianMonster@users.noreply.github.com>
2022-01-26 10:20:06 +0800

    Update base.h

--- include/cgen-vm/base.h
+++ include/cgen-vm/base.h
@@ -1,62 +1,21 @@
-#ifndef __CGEN_VM_BASE
-#define __CGEN_VM_BASE
-#include "api_def.h"
+#ifndef __CGEN_INCLUDE_BASE
+#define __CGEN_INCLUDE_BASE
+
 #include <stdint.h>
 
-/* Registers
- *
- * Registers:
- *   Common 8bit registers:
- * 		r8_a   - 8bit register  				#1
- * 		r8_b   - 8bit register  				#2
- * 		r8_c   - 8bit register  				#3
- * 		r8_d   - 8bit register  				#4
- *
- *   2nd 8bit registers:
- *   	r8_a2  - 8bit register  				#5
- *   	r8_b2  - 8bit register  				#6
- * 
- * 	 Special 8bit registers:
- *   	r8_cd  - conditional register  			#7
- *   	r8_cr  - condition result register  	#8
- *   
- * 	 16 bit registers: 8
- * 	  Only has general purpose registers
- * 	 32 bit registers: 8
- * 	  Only has general purpose registers
- * 
- *   Common 64bit registers:
- *   	r64_a  - 64bit register 				#1
- *   	r64_b  - 64bit register 				#2
- *   	r64_c  - 64bit register 				#3
- *   	r64_d  - 64bit register 				#4
- *
- *   Special 64bit registers:
- *   	r64_pc - 64bit program counter 			#6
- *   	r64_ax - accumulator register 			#5
- *   
- *   Secondary 64bit registers:
- *   	r64_a2 - 64bit register					#7
- *   	r64_b2 - 64bit register					#8
- */
-
-
-;
-
-/* CGen VM structure */
 typedef struct CGEN_VM_STRUCT
 {
-	unsigned char* memory; /* CGen VM memory */ 
-	unsigned long long memory_size; /* CGen VM memory size */
-	unsigned long long program_memory_address; /* CGen VM program memory address */
-	unsigned char register_mode; /* CGen VM register mode | 0 = 8 bit mode, 1 = 64 bit mode, 2 = 16 bit mode, 3 = 32 bit mode */
-	unsigned char* registers_8bit; /* CGen VM 8 bit registers */
-	uint16_t* registers_16bit; /* CGen VM 16 bit registers */
-	uint32_t* registers_32bit; /* CGen VM 32 bit registers */
-	unsigned long long* registers_64bit; /* CGen VM 64 bit registers */
-	int(**interrupts)(struct CGEN_VM_STRUCT*); /* CGen VM 256 Interrupts */
-} CGEN_VM;
+    uint8_t* mem; // VM memory
+    uint64_t mem_size; // VM memory size
 
-typedef int(*CGEN_INTERRUPT)(CGEN_VM*);
+    uint8_t reg_mode; // VM register mode
+
+    uint8_t* regs_8bit; // VM 8 bit registers
+    uint16_t* regs_16bit; // VM 16 bit registers
+    uint32_t* regs_32bit; // VM 32 bit registers
+    uint64_t* regs_64bit; // VM 64 bit registers
+
+    int(**interrupts)(struct CGEN_VM_STRUCT*); // VM interrupts
+} CGen_VM;
 
 #endif
