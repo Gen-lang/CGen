714c45fb7ca41d018e50f09ac2345f023cbc5bd9
GalaxianMonster <72903778+GalaxianMonster@users.noreply.github.com>
2022-01-26 10:24:22 +0800

    Update main.c

--- src/cgen-vm/main.c
+++ src/cgen-vm/main.c
@@ -1,6 +1,5 @@
-/* CGen VM test */
-
 #include <stdio.h>
+#include <cgen-vm/base.h>
 #include <cgen-vm/vm.h>
 #include <cgen-vm/instr.h>
 
@@ -28,8 +27,8 @@
 #define REG 0x80
 #define IMM 0x40
 
-#define R0(r) r << 5
-#define R1(r) r << 2
+#define R0(r) r << 4
+#define R1(r) r
 
 #define MiB(x) (1024 * 1024 * x)
 #define KiB(x) (1024 * x)
@@ -37,55 +36,55 @@
 #define VM_CYCLE_MAX KiB(1)
 #define VM_DEBUG_CYCLE 1
 
-void debug_registers(CGEN_VM* instance)
+void debug_registers(CGen_VM* instance)
 {
     printf("\n------ CGen VM Register Information ------\n\n");
-    printf("Register mode: %u\n\n", instance->register_mode);
-    for(int i = 0; i < 8; i++)
+    printf("Register mode: %u\n\n", instance->reg_mode);
+    for(int i = 0; i < 16; i++)
     {
-        printf("R8 #%d: %u\n", i, instance->registers_8bit[i]);
+        printf("R8 #%d: %u\n", i, instance->regs_8bit[i]);
     }
     printf("\n");
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < 16; i++)
     {
-        printf("R16 #%d: %u\n", i, instance->registers_16bit[i]);
+        printf("R16 #%d: %u\n", i, instance->regs_16bit[i]);
     }
     printf("\n");
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < 16; i++)
     {
-        printf("R32 #%d: %u\n", i, instance->registers_32bit[i]);
+        printf("R32 #%d: %u\n", i, instance->regs_32bit[i]);
     }
     printf("\n");
-    for(int i = 0; i < 8; i++)
+    for(int i = 0; i < 16; i++)
     {
-        printf("R64 #%d: %u\n", i, instance->registers_64bit[i]);
+        printf("R64 #%d: %llu\n", i, instance->regs_64bit[i]);
     }
     printf("\n------ CGen Register Information End ------\n\n");
 }
 
-int vm_int_pru(CGEN_VM* vm)
+int vm_int_pru(CGen_VM* vm)
 {
-    printf("%llu\n", vm->registers_64bit[vm->registers_8bit[1]]);
+    printf("%llu\n", vm->regs_64bit[vm->regs_8bit[1]]);
     return 1;
 }
 
-int vm_debug_exec(unsigned char* p, unsigned long long psize, const char* test_name)
+int vm_debug_exec(uint8_t* p, uint64_t psize, const char* test_name)
 {
     int res = 0;
     int cycle = 1;
     printf("---- Initialize CGen %s ----\n\n", test_name);
-    CGEN_VM vm;
-    res = CGEN_VM_init(p, psize, 0, psize, &vm);
+    CGen_VM vm;
+    res = CGen_VM_init(p, psize, 0, psize, &vm);
     if(res < 0)
     {
         printf("Error while initializing CGen %s\n", test_name);
         return -1;
     }
-    vm.interrupts[0] = (CGEN_INTERRUPT)&vm_int_pru;
+    vm.interrupts[0] = &vm_int_pru;
     while(1)
     {
         printf("CGen VM Cycle: #%d\n", cycle);
-        res = CGEN_VM_exec_instr(&vm);
+        res = CGen_VM_exec_instr(&vm);
         printf("CGen cycle result: %d\n\n", res);
         if((cycle % VM_DEBUG_CYCLE) == 0)
         {
@@ -106,8 +105,9 @@ int vm_debug_exec(unsigned char* p, unsigned long long psize, const char* test_n
         }
         cycle += 1;
     }
-    CGEN_VM_free(&vm);
+    CGen_VM_free(&vm);
     printf("---- CGen %s Done ----\n\n", test_name);
+    return 0;
 }
 
 int main()
@@ -123,12 +123,12 @@ int main()
         MOV | REG | IMM, R0(7), 0x01, // mov r8_cr, 0x01
         RMOD | IMM, 0x01, // rmod 0x01
         JMP | IMM, 0x05,  // jmp 0x05
-        0x00, 0x00, 0x00, 0x00, 
+        0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x00,
         EXIT, // exit
     };
     vm_debug_exec(p0, 15, "Test 1 : Infinite Loop");
-    
+
     unsigned char p1[] = {
         /*
          * Assembly version:
@@ -146,8 +146,8 @@ int main()
         0x00, 0x00,
         0x00, 0x00,
         0x00, 0x00,
-        RMOD | REG, r0(0), // rmod 0x00
-        MOV, // mov r8_a, 0
+        RMOD | IMM, 0,
+        MOV | REG | IMM, R0(0), 0,
         MOV | REG | IMM, R0(1), 4, // mov r8_b, 4
         INT, // int
         EXIT, // exit
