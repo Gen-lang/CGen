6011922145f378b78368b33e1987cd5b4a26aa74
bichanna <nobu.bichanna@gmail.com>
2022-01-27 21:27:11 -0600

    Forgot to add types

--- include/cgen-compiler/lexer.h
+++ include/cgen-compiler/lexer.h
@@ -1,8 +1,8 @@
 #ifndef __CGEN_COMPILER_LEXER
 #define __CGEN_COMPILER_LEXER
 
-extern const DIGITS;
-extern const LETTERS;
-extern const LETTERS_AND_DIGITS;
+extern const char DIGITS[];
+extern const char LETTERS[];
+extern const char LETTERS_AND_DIGITS[];
 
 #endif
